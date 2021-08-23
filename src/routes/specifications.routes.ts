dcb3d549acc8fcd3075b463d7033a081a41af608
Nathan V Marcelino <nathan.v.marcelino@gmail.com>
2021-08-23 20:03:01 -0300

    refactor(specification) - change wrong name on specification router

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -3,12 +3,12 @@ import { Router } from "express";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
-const specificarionsRoutes = Router();
+const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificarionsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post("/", createSpecificationController.handle);
 
-specificarionsRoutes.get("/", listSpecificationsController.handle);
-export { specificarionsRoutes };
+specificationsRoutes.get("/", listSpecificationsController.handle);
+export { specificationsRoutes };
